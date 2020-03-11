f32668d8d7deb4594831ea442b5e60d7b6aa99ef
Hugh Wang <18501667737@163.com>
2020-03-11 11:56:28 +0800

    by bash

---
+++ src/main/java/me/hughwang/shirotesting/ShiroTestingApplication.java
@@ -0,0 +1,13 @@
+package me.hughwang.shirotesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroTestingApplication.class, args);
+    }
+
+}
